515c5899603f55b70fef7a2f20b64df1dda6f020
Melvinsantos <47260944+Melvinsantos@users.noreply.github.com>
2021-12-09 19:40:49 -0600

    Primer fronted

---
+++ Practica1Progra2/public_html/Conector.js
@@ -0,0 +1,10 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+function datosIniciosesion(){
+    let usuario= document.getElementsByName('usuario');
+    console.log("usuario: "+usuario);
+}
+
